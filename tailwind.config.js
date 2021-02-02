37525135ed40a0c8918da0195d6c65b11655b63e
JordanGMaria <jordan@bestuse.com.br>
2021-02-01 23:43:38 -0300

    icons e font youtube

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,17 +2,20 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    backgroundColor: theme => ({
-     ...theme('colors'),
-     'primary': '#181818',
-     'secondary': '#212121',
-     'tertiary': '#323232',
-     'quaternary': '#383838',
-     'quintenary': '#424242',
-    })
+    backgroundColor: (theme) => ({
+      ...theme("colors"),
+      primary: "#181818",
+      secondary: "#212121",
+      tertiary: "#323232",
+      quaternary: "#383838",
+      quintenary: "#424242",
+    }),
+    fontFamily: {
+      roboto: ["Roboto"],
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
